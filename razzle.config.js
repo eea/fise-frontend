73ae3c75a5f59ed50f039b4b04bea236d50c5000
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-10-24 09:55:37 +0300

    Avoid hardcoding volto-mosaic path

--- razzle.config.js
+++ razzle.config.js
@@ -1,9 +1,6 @@
-/**
- * Replace with custom razzle config when needed.
- * @module razzle.config
- */
+// const { ReactLoadablePlugin } = require('react-loadable/webpack');
 
-const { ReactLoadablePlugin } = require('react-loadable/webpack');
+const resolve = require('path').resolve;
 const jsConfig = require('./jsconfig').compilerOptions;
 
 const pathsConfig = jsConfig.paths;
@@ -22,8 +19,7 @@ module.exports = {
 
   modify: (config, { target, dev }, webpack) => {
     const vc = voltoConfig.modify(config, { target, dev }, webpack);
-
-    vc.module.rules[0].include.push('./volto-mosaic');
+    vc.module.rules[0].include.push(resolve('./volto-mosaic'));
 
     // need to include /theme/ to less loader in order to have it working with volto as a submodule.
     const modifiedLess = vc.module.rules.find(
@@ -34,12 +30,12 @@ module.exports = {
     vc.module.rules[index] = modifiedLess;
 
     console.log('vc', vc);
-    vc.plugins = [
-      ...vc.plugins,
-      new ReactLoadablePlugin({
-        filename: './build/react-loadable.json',
-      }),
-    ];
+    // vc.plugins = [
+    //   ...vc.plugins,
+    //   new ReactLoadablePlugin({
+    //     filename: './build/react-loadable.json',
+    //   }),
+    // ];
 
     return vc;
   },
