5721b843881c9b28143b53b994c08bb33b29163c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-26 18:03:56 +0200

    Include volto-sidebar

--- src/config.js
+++ src/config.js
@@ -10,11 +10,13 @@ import { applyConfig as ckeditorConfig } from 'volto-ckeditor/config';
 import { applyConfig as mosaicConfig } from 'volto-mosaic/config';
 import { applyConfig as dataBlocksConfig } from 'volto-datablocks/config';
 import { applyConfig as installFiseFrontend } from './localconfig';
+import { applyConfig as installSidebar } from 'volto-sidebar/config';
 
 const config = [
   addonsConfig,
-  installImageSlides,
+  installSidebar,
   installPortlets,
+  installImageSlides,
   plotlyConfig,
   ckeditorConfig,
   // draftConfig,
@@ -39,6 +41,9 @@ export const blocks = {
   ...config.blocks,
 };
 
+// TODO: should we move custom stuff to settings variable?
+// It would make future adding new settings types easier, as this file wouldn't
+// have to be updated in all frontend implementations
 export const addonReducers = { ...config.addonReducers };
 export const addonRoutes = [...(config.addonRoutes || [])];
 
@@ -46,4 +51,8 @@ export const viewlets = [...(config.viewlets || [])];
 
 export const portlets = {
   ...config.portlets,
+};
+
+export const editForms = {
+  ...config.editForms,
 };
