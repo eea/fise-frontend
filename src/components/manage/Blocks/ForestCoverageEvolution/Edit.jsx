172aa555dacad0dda4598372c8fe6acb4661b625
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-11 23:19:07 +0200

    WIP on evolution block

--- src/components/manage/Blocks/ForestCoverageEvolution/Edit.jsx
+++ src/components/manage/Blocks/ForestCoverageEvolution/Edit.jsx
@@ -1,2 +1,18 @@
+import React from 'react';
 import Edit from 'volto-plotlycharts/EmbedChartBlock/Edit';
-export default Edit;
+
+const SCHEMA = {
+  total: {
+    title: 'Deadwood volume column',
+    defaultformat: 'compactnumber',
+  },
+  totalA: {
+    title: 'Deadwood volume columnA',
+    defaultformat: 'compactnumber',
+  },
+};
+
+const EditBlock = props => (
+  <Edit title="Forest Coverage Evolution" schema={SCHEMA} {...props} />
+);
+export default EditBlock;
