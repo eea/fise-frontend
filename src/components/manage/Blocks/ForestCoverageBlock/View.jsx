55428e5fff83859916dd397c5d4d679024caf1fa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-12 19:12:57 +0200

    Sync

--- src/components/manage/Blocks/ForestCoverageBlock/View.jsx
+++ src/components/manage/Blocks/ForestCoverageBlock/View.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DataConnectedValue from 'volto-datablocks/DataConnectedValue';
 
 const View = props => {
+  console.log('block props', props);
   return (
     <div className="block-container">
       <div className="forest-block-wrapper">
@@ -10,27 +11,23 @@ const View = props => {
           <div className="land-data-wrapper eu28-data">
             <div className="land-data">
               <span>
-                {props.data.url && props.data.columns?.perc && (
-                  <DataConnectedValue
-                    url={props.data.provider_url}
-                    column={props.data.columns.perc.value}
-                    format={props.data.columns.perc.format}
-                    placeholder="_"
-                  />
-                )}
+                <DataConnectedValue
+                  url={props.data.provider_url}
+                  column={props.data.columns.perc?.value}
+                  format={props.data.columns.perc?.format}
+                  placeholder="_"
+                />
               </span>
             </div>
             <div className="land-data-content">
               of land surface
               <span>
-                {props.data.url && props.data.columns?.totalArea && (
-                  <DataConnectedValue
-                    url={props.data.provider_url}
-                    column={props.data.columns.totalArea.value}
-                    format={props.data.columns.totalArea.format}
-                    placeholder="_"
-                  />
-                )}{' '}
+                <DataConnectedValue
+                  url={props.data.provider_url}
+                  column={props.data.columns.totalArea?.value}
+                  format={props.data.columns.totalArea?.format}
+                  placeholder="_"
+                />{' '}
                 ha
               </span>
             </div>
