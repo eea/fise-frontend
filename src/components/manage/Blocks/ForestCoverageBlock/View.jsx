8467ae36edb5e84e1c0e0fc35393f8ee56a984f1
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-19 23:07:47 +0200

    Remove one level of block-container class

--- src/components/manage/Blocks/ForestCoverageBlock/View.jsx
+++ src/components/manage/Blocks/ForestCoverageBlock/View.jsx
@@ -4,39 +4,37 @@ import DataConnectedValue from 'volto-datablocks/DataConnectedValue';
 const View = props => {
   console.log('block props', props);
   return (
-    <div className="block-container">
-      <div className="forest-block-wrapper">
-        <div className="forest-specific-block forest-area-block">
-          {props.data?.block_title ? <h5>{props.data.block_title}</h5> : ''}
-          <div className="land-data-wrapper eu28-data">
-            <div className="land-data">
-              <span>
-                <DataConnectedValue
-                  url={props.data.provider_url}
-                  column={props.data?.columns?.perc?.value}
-                  format={props.data?.columns?.perc?.format}
-                  placeholder="_"
-                />
-              </span>
-            </div>
-            <div className="land-data-content">
-              {props.data?.columns?.percText?.value}
-              <span>
-                <DataConnectedValue
-                  url={props.data.provider_url}
-                  column={props.data?.columns?.totalArea?.value}
-                  format={props.data?.columns?.totalArea?.format}
-                  placeholder="_"
-                />{' '}
-                {props.data?.columns?.totalAreaUnit?.value}
-              </span>
-            </div>
+    <div className="forest-block-wrapper">
+      <div className="forest-specific-block forest-area-block">
+        {props.data?.block_title ? <h5>{props.data.block_title}</h5> : ''}
+        <div className="land-data-wrapper eu28-data">
+          <div className="land-data">
+            <span>
+              <DataConnectedValue
+                url={props.data.provider_url}
+                column={props.data?.columns?.perc?.value}
+                format={props.data?.columns?.perc?.format}
+                placeholder="_"
+              />
+            </span>
           </div>
-          <div>
-            <a className="discreet" href={props.data.chart_source_link}>
-              {props.data.chart_source}
-            </a>
+          <div className="land-data-content">
+            {props.data?.columns?.percText?.value}
+            <span>
+              <DataConnectedValue
+                url={props.data.provider_url}
+                column={props.data?.columns?.totalArea?.value}
+                format={props.data?.columns?.totalArea?.format}
+                placeholder="_"
+              />{' '}
+              {props.data?.columns?.totalAreaUnit?.value}
+            </span>
           </div>
+        </div>
+        <div>
+          <a className="discreet" href={props.data.chart_source_link}>
+            {props.data.chart_source}
+          </a>
         </div>
       </div>
     </div>
