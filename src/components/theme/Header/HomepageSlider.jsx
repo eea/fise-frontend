023fd2a8c7662df6eae30028f1c61b0371276cc4
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-21 22:04:26 +0200

    slider redesign with smaller height

--- src/components/theme/Header/HomepageSlider.jsx
+++ src/components/theme/Header/HomepageSlider.jsx
@@ -1,50 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { Icon } from '@plone/volto/components';
-
-// import Slider from 'react-slick';
-import left from '@plone/volto/icons/left-key.svg';
-import right from '@plone/volto/icons/right-key.svg';
-
-// Import css files
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
-// import SliderCaret from './slidercarret.svg';
-
 import 'react-image-gallery/styles/css/image-gallery.css';
 
 import ImageGallery from 'react-image-gallery';
-
-// function SampleNextArrow(props) {
-//   const { onClick } = props;
-//   return (
-//     <div
-//       role="button"
-//       className="slideArrow nextArrow"
-//       onClick={onClick}
-//       tabIndex={-1}
-//       onKeyPress={() => {}}
-//     >
-//       <Icon name={right} size="45px" />
-//     </div>
-//   );
-// }
-
-// function SamplePrevArrow(props) {
-//   const { onClick } = props;
-//   return (
-//     <div
-//       role="button"
-//       className="slideArrow prevArrow"
-//       onClick={onClick}
-//       tabIndex={-1}
-//       onKeyPress={() => {}}
-//     >
-//       <Icon name={left} size="45px" />
-//     </div>
-//   );
-// }
 
 class HomepageSlider extends Component {
   constructor(props) {
@@ -63,19 +22,6 @@ class HomepageSlider extends Component {
         description: PropTypes.string,
       }),
     ),
-  };
-
-  renderThumbnail = item => {
-    return (
-      <div className="slider-thumbnail" key={item.original}>
-        {/* <img src={item.original} /> */}
-        <div
-          className="thumbnail-img"
-          style={{ backgroundImage: `url(${item.original})` }}
-        />
-        <div className="slide-title">{item.title}</div>
-      </div>
-    );
   };
 
   renderSlide = item => {
@@ -124,8 +70,7 @@ class HomepageSlider extends Component {
 
   componentDidUpdate(prevProps) {
     if (
-      !this.state.slides.length 
-      &&
+      !this.state.slides.length &&
       this.props.items &&
       this.props.items.length
     ) {
@@ -137,26 +82,6 @@ class HomepageSlider extends Component {
     if (!this.state.slides.length) return '';
     return (
       <div className="slider-wrapper">
-        {/* <Slider
-          className="mainSlider"
-          asNavFor={this.state.nav2}
-          ref={slider => (this.slider1 = slider)}
-          {...settings}
-        >
-          {this.state.slides}
-        </Slider>
-        <Slider
-          className="navSlider"
-          asNavFor={this.state.nav1}
-          ref={slider => (this.slider2 = slider)}
-          slidesToShow={3}
-          slidesToScroll={1}
-          swipeToSlide={true}
-          focusOnSelect={true}
-        >
-          {this.state.slides}
-        </Slider> */}
-        {/* <img className="slider-caret" src={SliderCaret} alt="" /> */}
         <ImageGallery
           className="mainSlider"
           items={this.state.slides}
@@ -164,22 +89,15 @@ class HomepageSlider extends Component {
           showPlayButton={false}
           autoPlay
           renderItem={this.renderSlide}
-          renderThumbInner={this.renderThumbnail}
+          // renderThumbInner={this.renderThumbnail}
+          showThumbnails={false}
+          showBullets={true}
           slideDuration={300}
-          slideInterval={120000}
+          slideInterval={258000}
         />
       </div>
     );
   }
 }
-
-// export default compose(
-//   connect(
-//     state => ({
-//       items: state.frontpage_slides.items,
-//     }),
-//     { getFrontpageSlides },
-//   ),
-// )(HomepageSlider);
 
 export default HomepageSlider;
