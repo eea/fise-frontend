5d0925db39c15266e8594c4bbef20bf537ce0c3f
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-27 17:54:58 +0200

    forest coverage design change

--- src/components/manage/Blocks/ForestCoverageBlock/View.jsx
+++ src/components/manage/Blocks/ForestCoverageBlock/View.jsx
@@ -20,7 +20,6 @@ const View = props => {
             </span>
           </div>
           <div className="land-data-content">
-            {props.data?.columns?.percText?.value}
             <span>
               <DataConnectedValue
                 url={props.data.provider_url}
@@ -30,6 +29,7 @@ const View = props => {
               />{' '}
               {props.data?.columns?.totalAreaUnit?.value}
             </span>
+            {props.data?.columns?.percText?.value}
           </div>
         </div>
         <div>
