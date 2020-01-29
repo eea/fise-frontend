40755362cd2bbdca67b886ec0a682c76dcba5f4f
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-30 00:33:27 +0200

    Width based grid

---
+++ src/components/theme/LayoutProvider/WidthBasedLayoutProvider.jsx
@@ -0,0 +1,26 @@
+import React, { Component, Children } from 'react';
+// import TableauReport from '~/components/theme/TableauView/TableauReport';
+import componentQueries from 'react-component-queries';
+import { compose } from 'redux';
+
+const WidthBasedLayoutProvider = WrappedComponent => props => {
+  return <WrappedComponent {...props} />;
+};
+
+export default compose(
+  componentQueries(({ width }) => ({
+    layout_type: (() => {
+      if (width > 1549) {
+        return 'widescreen';
+      }
+      if (width > 1086) {
+        return 'desktop';
+      }
+      if (width > 768) {
+        return 'tablet';
+      }
+      return 'mobile';
+    })(),
+  })),
+  WidthBasedLayoutProvider,
+);
