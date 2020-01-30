6ab2d44bad345d7a5fec899bdc42d693f7b377ef
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-30 11:43:14 +0200

    Added matomo, disabled for now

--- src/start-client.jsx
+++ src/start-client.jsx
@@ -11,14 +11,24 @@ import '~/theme';
 
 import configureStore from '@plone/volto/store';
 import { Api, persistAuthToken, ScrollToTop } from '@plone/volto/helpers';
+// import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 
 export const history = createBrowserHistory();
+
+// const matomo = createInstance({
+//   urlBase: 'https://matomo.eea.europa.eu/',
+//   siteId: 46, // optional, default value: `1`
+//   // trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
+//   // srcUrl: 'https://LINK.TO.DOMAIN/tracking.js', // optional, default value: `${urlBase}matomo.js`
+// });
 
 export default () => {
   const api = new Api();
 
   const store = configureStore(window.__data, history, api);
   persistAuthToken(store);
+  //      <MatomoProvider value={matomo}>
+  //      </MatomoProvider>
 
   hydrate(
     <Provider store={store}>
