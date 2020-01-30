8936d528ad913ab003aef72df3f14c415234675f
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-30 16:06:45 +0200

    consistent breakpoints

--- src/components/theme/LayoutProvider/WidthBasedLayoutProvider.jsx
+++ src/components/theme/LayoutProvider/WidthBasedLayoutProvider.jsx
@@ -1,5 +1,4 @@
-import React, { Component, Children } from 'react';
-// import TableauReport from '~/components/theme/TableauView/TableauReport';
+import React from 'react';
 import componentQueries from 'react-component-queries';
 import { compose } from 'redux';
 
@@ -10,13 +9,13 @@ const WidthBasedLayoutProvider = WrappedComponent => props => {
 export default compose(
   componentQueries(({ width }) => ({
     layout_type: (() => {
-      if (width > 1549) {
+      if (width > 1600) {
         return 'widescreen';
       }
-      if (width > 1086) {
+      if (width > 1200) {
         return 'desktop';
       }
-      if (width > 768) {
+      if (width > 767) {
         return 'tablet';
       }
       return 'phone';
@@ -24,3 +23,6 @@ export default compose(
   })),
   WidthBasedLayoutProvider,
 );
+/* 767        768-1199      1200-1599     1600+
+mobile     tablet         desktop       widescreen */
+/* export const breakpoints = { lg: 1600, md: 1200, sm: 768, xs: 0, xxs: 0 }; */
