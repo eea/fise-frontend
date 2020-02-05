89104425a137105eb6c4e542ffe6423670b68cc2
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-02-05 17:23:38 +0200

    Improve build

--- scripts/prebuild-fix-modules.sh
+++ scripts/prebuild-fix-modules.sh
@@ -10,30 +10,42 @@ NODE_MODULES=./node_modules
 
 # declare remove inner packages as package.json:optimizeLiftPackages
 REMOVE_INNER_PACKAGES=`./scripts/print_key.js optimizeLiftPackages`
-read -ra REMOVE_PKGS <<< "$REMOVE_INNER_PACKAGES"
+read -ra REMOVE <<< "$REMOVE_INNER_PACKAGES"
 
 # declare remove inner packages as package.json:optimizeLiftPackages
 TRANSPILE_PACKAGES=`./scripts/print_key.js manuallyTranspile`
-read -ra REMOVE_PKGS <<< "$REMOVE_INNER_PACKAGES"
+read -ra TRANSPILE <<< "$TRANSPILE_PACKAGES"
 
 # Locate inner packages in ADDONS_DIR and NODE_MODULES, remove them
 # Equivalent of doing:
 # find node_modules | grep "node_modules/.*/node_modules/immutable$" | xargs rm -r
 
-# optimize lifted packages in addons
-for f in ${ADDONS_DIR}*/; do
-  for PKG in "${REMOVE_PKGS[@]}"; do
-    echo Will cleanup "${PKG}"
+# optimize lifted packages in addons and node_modules
+for PKG in "${REMOVE[@]}"; do
+  echo Cleaning up ${PKG}
+
+  for f in ${ADDONS_DIR}*/; do
     find $f | grep "node_modules/${PKG}$" | xargs echo
   done;
+
+  find ${NODE_MODULES} | grep "node_modules/.*/node_modules/${PKG}$" | xargs echo
+
 done;
 
-# optimize lifted packages in main node_modules
-for PKG in "${REMOVE_PKGS[@]}"; do
-  echo Will cleanup "${PKG}"
-  find $1 | grep "node_modules/.*/node_modules/${PKG}$" | xargs echo
-done
 
+for PKG in "${TRANSPILE[@]}"; do
+  echo Transpiling ${PKG}
+
+  for f in ${ADDONS_DIR}*/; do
+    find $f | \
+      grep "node_modules/${PKG}$" | \
+      xargs -I % ./node_modules/.bin/babel --presets="@babel/env" % --out-dir %
+  done;
+
+  find ${NODE_MODULES} | \
+    grep "node_modules/.*/node_modules/${PKG}$" | \
+    xargs -I % ./node_modules/.bin/babel --presets="@babel/env" % --out-dir %
+done
 
 
 # find node_modules | grep "node_modules/prepend-http$" | xargs -I % ./node_modules/.bin/babel --presets="@babel/env" % --out-dir %
