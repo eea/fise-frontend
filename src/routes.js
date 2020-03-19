6309b3fb9be333bead78a655397dd83e53b73fc6
Miu Razvan <miu.razvan28@gmail.com>
2020-03-19 12:27:00 +0200

    Server responds with status 404 and 401 for NotFound and Unauthorized pages

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@
 import { App } from '@plone/volto/components';
 import { defaultRoutes } from '@plone/volto/routes';
 import HomepageView from '~/components/theme/HomepageView/HomepageView';
+import NotFound from '~/customizations/volto/components/theme/NotFound/NotFound';
+import Contents from '~/customizations/volto/components/theme/Contents/Contents';
 
 import { addonRoutes } from '~/config';
 import SiteMap from '~/components/theme/SiteMap/SiteMap';
@@ -30,6 +32,14 @@ const routes = [
         path: '/sitemap',
         component: SiteMap,
         exact: true,
+      },
+      {
+        path: '/contents',
+        component: Contents,
+      },
+      {
+        path: '/**/contents',
+        component: Contents,
       },
       // addon routes have a higher priority then default routes
       ...(addonRoutes || []),
