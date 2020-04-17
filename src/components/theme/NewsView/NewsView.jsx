c1faf01cab95e7c519764bd4fd4e4c1ad48335ac
Mihai Macaneata <themacaneata@gmail.com>
2020-04-17 12:09:33 +0300

    Allow only newsitems in news view

--- src/components/theme/NewsView/NewsView.jsx
+++ src/components/theme/NewsView/NewsView.jsx
@@ -19,52 +19,72 @@ class NewsView extends Component {
       tablet: 'twelve',
       desktop: 'twelve',
       widescreen: 'twelve',
-    }
-  }
+    },
+  };
 
   componentDidMount() {
-    const items = this.props.content.items.map(item => ({
-      id: item['@id'],
-      date: item.effective,
-      type: item['@type'],
-      title: item.title,
-      image: item.image ? item.image.download : null,
-      description: item.description,
-      text: item.text,
-      topics: item.topics,
-      blocks: item.blocks,
-      blocks_layout: item.blocks_layout
-    })).sort((a, b) => {
-      return new Date(b.date) - new Date(a.date) 
-    });
-    this.setState({ items })
+    const items = this.props.content.items
+      .map(item => ({
+        id: item['@id'],
+        date: item.effective,
+        type: item['@type'],
+        title: item.title,
+        image: item.image ? item.image.download : null,
+        description: item.description,
+        text: item.text,
+        topics: item.topics,
+        blocks: item.blocks,
+        blocks_layout: item.blocks_layout,
+      }))
+      .sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+      })
+      .filter(item => item.type === 'News Item');
+    this.setState({ items });
   }
 
   render() {
-    if (!this.state.items) return (<h1>News</h1>);
+    if (!this.state.items) return <h1>News</h1>;
     return (
       <Container>
         <Helmet title="News" />
         <div className="news-page-content">
           <BodyClass />
-          <Label className="rss-feed" as='a' size="large" href={settings.apiPath + '/news/RSS'} target="_blank" color="teal">
+          <Label
+            className="rss-feed"
+            as="a"
+            size="large"
+            href={settings.apiPath + '/news/RSS'}
+            target="_blank"
+            color="teal"
+          >
             <span>Subscribe via RSS</span>
             <Icon name={rss} size="14px" />
           </Label>
-          <div className={`news-wrapper-view ${this.props.layout_type}-${this.state.grid[this.props.layout_type]}`}>
-            { this.state.items && this.state.items.map((item, index) => {
-              if (index < this.state.show) return (<NewsItem key={item.id} item={item}  />)
-            })}
+          <div
+            className={`news-wrapper-view ${this.props.layout_type}-${
+              this.state.grid[this.props.layout_type]
+            }`}
+          >
+            {this.state.items &&
+              this.state.items.map((item, index) => {
+                if (index < this.state.show)
+                  return <NewsItem key={item.id} item={item} />;
+              })}
           </div>
-          { this.state.items.length > this.state.limit && this.state.show <= this.state.limit && (
-              <button className="news-expand-button" onClick={() => { this.setState({ show: this.state.items.length }) }}>
+          {this.state.items.length > this.state.limit &&
+            this.state.show <= this.state.limit && (
+              <button
+                className="news-expand-button"
+                onClick={() => {
+                  this.setState({ show: this.state.items.length });
+                }}
+              >
                 <span />
                 <Icon name={downKey} size="60px" color="#E7E7E7" />
                 <span />
               </button>
-            )
-          }
-          
+            )}
         </div>
       </Container>
     );
