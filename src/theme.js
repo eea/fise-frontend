b91a62ed8a90eb764b70ef34e0099122e9b9bbf8
Mihai Macaneata <themacaneata@gmail.com>
2020-04-22 14:48:00 +0300

    new theme structure

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,2 @@
 import 'semantic-ui-less/semantic.less';
-// import '@plone/volto/../theme/theme.less';
-// import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
-import '../theme/themes/forest/extras/extras.less';
+import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
