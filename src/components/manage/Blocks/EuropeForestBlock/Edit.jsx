8d0e725863b67f1af9cca39c37b0b618a527b5c0
Mihai Macaneata <themacaneata@gmail.com>
2020-07-24 11:28:32 +0300

    rever europe forest area block to unconnected block

--- src/components/manage/Blocks/EuropeForestBlock/Edit.jsx
+++ src/components/manage/Blocks/EuropeForestBlock/Edit.jsx
@@ -1,25 +1,128 @@
-import React, { useState } from 'react';
-import _uniqueId from 'lodash/uniqueId';
-import DefaultEdit from '../DefaultEdit';
-import View from './View';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import React, { Component } from 'react';
+import { Form as UiForm } from 'semantic-ui-react';
+import { Field } from '@plone/volto/components'; // EditBlock
 
-const schema = require('./schema.json');
-const Edit = props => {
-  const [state] = useState({
-    id: _uniqueId('block_'),
-  });
-  return (
-    <div>
-      <DefaultEdit {...props} id={state.id} schema={schema} />
-      <View {...props} id={state.id} />
-    </div>
-  );
-};
-export default compose(
-  connect((state, props) => ({
-    connected_data_parameters: state.connected_data_parameters,
-    pathname: state.router.location.pathname,
-  })),
-)(Edit);
+class Edit extends Component {
+  constructor(props) {
+    super(props);
+
+    const blockData = props.data;
+
+    this.state = {
+      ...blockData,
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.updateData = this.updateData.bind(this);
+  }
+
+  updateData(obj) {
+    this.setState(obj, this.onSubmit);
+  }
+
+  onSubmit() {
+    this.props.onChangeBlock(this.props.block, {
+      ...this.props.data,
+      ...this.state,
+    });
+  }
+
+  render() {
+    return (
+      <div className="block selected">
+        <div className="block-inner-wrapper">
+          <UiForm>
+            <Field
+              id="europe-block-title"
+              title="Title"
+              type="text"
+              value={this.state.europe_block_title}
+              required={false}
+              onChange={(e, d) => this.updateData({ europe_block_title: d })}
+            />
+
+            <Field
+              id="europe-forest-p-eu28"
+              title="Forest area in EU28"
+              type="text"
+              value={this.state.europe_forest_p_eu28}
+              required={false}
+              onChange={(e, d) => this.updateData({ europe_forest_p_eu28: d })}
+            />
+
+            <Field
+              id="europe-forest-p-eu28-text"
+              title="Forest area in EU28 TEXT"
+              type="text"
+              value={this.state.europe_forest_p_eu28_text}
+              required={false}
+              onChange={(e, d) =>
+                this.updateData({ europe_forest_p_eu28_text: d })
+              }
+            />
+
+            <Field
+              id="europe-forest-l-eu28"
+              title="Total forest area in EU28"
+              type="text"
+              value={this.state.europe_forest_l_eu28}
+              required={false}
+              onChange={(e, d) => this.updateData({ europe_forest_l_eu28: d })}
+            />
+
+            <Field
+              id="europe-forest-p-eea39"
+              title="Forest area in EEA39"
+              type="text"
+              value={this.state.europe_forest_p_eea39}
+              required={false}
+              onChange={(e, d) => this.updateData({ europe_forest_p_eea39: d })}
+            />
+
+            <Field
+              id="europe-forest-p-eea39-text"
+              title="Forest area in EEA39 TEXT"
+              type="text"
+              value={this.state.europe_forest_p_eea39_text}
+              required={false}
+              onChange={(e, d) =>
+                this.updateData({ europe_forest_p_eea39_text: d })
+              }
+            />
+
+            <Field
+              id="europe-forest-l-eea39"
+              title="Total forest area in EEA39"
+              type="text"
+              value={this.state.europe_forest_l_eea39}
+              required={false}
+              onChange={(e, d) => this.updateData({ europe_forest_l_eea39: d })}
+            />
+
+            <Field
+              id="europe-source"
+              title="Source"
+              type="text"
+              value={this.state.europe_text_attribution}
+              required={false}
+              onChange={(e, d) =>
+                this.updateData({ europe_text_attribution: d })
+              }
+            />
+
+            <Field
+              id="europe-block-link"
+              title="Source link"
+              type="text"
+              value={this.state.europe_block_link}
+              required={false}
+              onChange={(e, d) => this.updateData({ europe_block_link: d })}
+            />
+          </UiForm>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Edit;
--- src/components/manage/Blocks/EuropeForestBlock/View.jsx
+++ src/components/manage/Blocks/EuropeForestBlock/View.jsx
@@ -1,89 +1,40 @@
-import React, { useState } from 'react';
-import DataConnectedValue from 'volto-datablocks/DataConnectedValue';
-import { SourceView } from 'volto-datablocks/Sources';
+import React, { Component } from 'react';
+// import TableauReport from '~/components/theme/TableauView/TableauReport';
 
-import DefaultView from '../DefaultView';
+class View extends Component {
+  render() {
+    return (
+        <div className="forest-block-wrapper">
+          <div className="forest-specific-block forest-area-block">
+            <h5>{this.props.data.europe_block_title}</h5>
 
-const schema = require('./schema.json');
-const View = props => {
-  const [state, setState] = useState({
-    onChange: newState => {
-      setState({ ...state, ...newState });
-    },
-  });
-  const view = (
-    <div className="forest-block-wrapper">
-      <div className="forest-specific-block forest-area-block">
-        {props.data?.block_title ? <h5>{props.data.block_title}</h5> : ''}
-
-        <div className="land-data-wrapper eu28-data">
-          <div className="land-data">
-            <span>
-              <DataConnectedValue
-                filterIndex={state.ids?.[0] || 0}
-                url={
-                  props.data?.providers?.['eu28_data_provider']?.path ||
-                  props.data?.provider_url
-                }
-                column={props.data?.columns?.eu28_total?.value}
-                format={props.data?.columns?.eu28_total?.format}
-                placeholder="_"
-              />
-            </span>
-          </div>
-          <div className="land-data-content">
-            <span>{props.data?.columns?.eu28_total_unit?.value}</span>
-            {props.data?.columns?.eu28_total_text?.value}
-          </div>
-        </div>
-
-        <div className="land-data-wrapper eea39-data">
-          <div className="land-data">
-            <span>
-              <div>
-                <DataConnectedValue
-                  filterIndex={state.ids?.[1] || 0}
-                  url={
-                    props.data?.providers?.['eea39_data_provider']?.path ||
-                    props.data?.provider_url
-                  }
-                  column={props.data?.columns?.eea39_total?.value}
-                  format={props.data?.columns?.eea39_total?.format}
-                  placeholder="_"
-                />
+            <div className="land-data-wrapper eu28-data">
+              <div className="land-data">
+                <span>{this.props.data.europe_forest_p_eu28}</span>
               </div>
-            </span>
-          </div>
-          <div className="land-data-content">
-            <span>{props.data?.columns?.eea39_total_unit?.value}</span>
-            {props.data?.columns?.eea39_total_text?.value}
-          </div>
-        </div>
+              <div className="land-data-content">
+                <span>{this.props.data.europe_forest_l_eu28}</span>
+                {this.props.data.europe_forest_p_eu28_text}{' '}
+              </div>
+            </div>
 
-        <div>
-          {props?.data?.chart_source &&
-            props?.data?.chart_source_link &&
-            props?.data?.chartSources &&
-            props?.data?.providers?.['eu28_data_provider'] && (
-              <SourceView
-                initialSource={props.data.chart_source}
-                initialSourceLink={props.data.chart_source_link}
-                multipleSources={props.data.chartSources}
-                providerUrl={props.data.providers['eu28_data_provider']}
-              />
-            )}
+            <div className="land-data-wrapper eea39-data">
+              <div className="land-data">
+                <span>{this.props.data.europe_forest_p_eea39}</span>
+              </div>
+              <div className="land-data-content">
+                <span>{this.props.data.europe_forest_l_eea39}</span>
+                {this.props.data.europe_forest_p_eea39_text}{' '}
+              </div>
+            </div>
+
+            <a className="discreet" href={this.props.data.europe_block_link}>
+              {this.props.data.europe_text_attribution}
+            </a>
+          </div>
         </div>
-      </div>
-    </div>
-  );
-  return (
-    <DefaultView
-      {...props}
-      schema={schema}
-      view={view}
-      onChange={state.onChange}
-    />
-  );
-};
+    );
+  }
+}
 
 export default View;
