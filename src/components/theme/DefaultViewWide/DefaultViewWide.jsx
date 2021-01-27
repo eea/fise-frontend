a8a9c1d73b19e856fcb1122b80a0a99e32a7f4f7
Mihai Macaneata <themacaneata@gmail.com>
2021-01-27 19:00:25 +0200

    default view styling

--- src/components/theme/DefaultViewWide/DefaultViewWide.jsx
+++ src/components/theme/DefaultViewWide/DefaultViewWide.jsx
@@ -8,6 +8,7 @@ import PropTypes from 'prop-types';
 import { Helmet } from '@plone/volto/helpers';
 import { injectIntl } from 'react-intl'; // defineMessages,
 import { Grid } from 'semantic-ui-react';
+import { BodyClass } from '@plone/volto/helpers';
 
 import { Container, Image } from 'semantic-ui-react';
 import { map } from 'lodash';
@@ -53,8 +54,11 @@ const DefaultViewWide = (props) => {
   //
   // if (!shouldRenderRoutes) return <Spinner />;
   return (
-    <Grid columns="equal" className="zero-margin">
-      {/* {renderPortletManager('plone.leftcolumn', 2, { ...props })}
+    <>
+      <BodyClass className={`document_wide_view`} />
+
+      <Grid columns="equal" className="zero-margin">
+        {/* {renderPortletManager('plone.leftcolumn', 2, { ...props })}
       <Grid.Column
         style={{ position: 'static' }}
         tablet={12}
@@ -62,71 +66,72 @@ const DefaultViewWide = (props) => {
         widescreen={6}
         computer={8}
       > */}
-      {hasBlocksData(content) ? (
-        <div id="page-document" className="ui container">
-          <Helmet title={content.title} />
-          {map(content[blocksLayoutFieldname].items, (block) => {
-            const Block =
-              blocks.blocksConfig[
-                content[blocksFieldname]?.[block]?.['@type']
-              ]?.['view'] || null;
-            return Block !== null &&
-              content[blocksFieldname][block]['@type'] !== 'title' ? (
-              <Block
-                key={block}
-                id={block}
-                properties={content}
-                data={content[blocksFieldname][block]}
-                path={getBaseUrl(location?.pathname || '')}
-              />
-            ) : (
-              //   <div key={block}>
-              //     {intl.formatMessage(messages.unknownBlock, {
-              //       block: content[blocksFieldname]?.[block]?.['@type'],
-              //     })}
-              //   </div>
-              ''
-            );
-          })}
-          <div id="forest-metadata-slot">{''}</div>
-        </div>
-      ) : (
-        <Container id="page-document">
-          {/* <Helmet title={content.title} />
+        {hasBlocksData(content) ? (
+          <div id="page-document" className="ui container">
+            <Helmet title={content.title} />
+            {map(content[blocksLayoutFieldname].items, (block) => {
+              const Block =
+                blocks.blocksConfig[
+                  content[blocksFieldname]?.[block]?.['@type']
+                ]?.['view'] || null;
+              return Block !== null &&
+                content[blocksFieldname][block]['@type'] !== 'title' ? (
+                <Block
+                  key={block}
+                  id={block}
+                  properties={content}
+                  data={content[blocksFieldname][block]}
+                  path={getBaseUrl(location?.pathname || '')}
+                />
+              ) : (
+                //   <div key={block}>
+                //     {intl.formatMessage(messages.unknownBlock, {
+                //       block: content[blocksFieldname]?.[block]?.['@type'],
+                //     })}
+                //   </div>
+                ''
+              );
+            })}
+            <div id="forest-metadata-slot">{''}</div>
+          </div>
+        ) : (
+          <Container id="page-document">
+            {/* <Helmet title={content.title} />
       <h1 className="documentFirstHeading">{content.title}</h1>
       {content.description && (
         <p className="documentDescription">{content.description}</p>
       )} */}
-          {content.image && (
-            <Image
-              className="document-image"
-              src={content.image.scales.thumb.download}
-              floated="right"
-            />
-          )}
-          {content.remoteUrl && (
-            <span>
-              The link address is:
-              <a href={content.remoteUrl}>{content.remoteUrl}</a>
-            </span>
-          )}
-          {content.text && (
-            <div
-              dangerouslySetInnerHTML={{
-                __html: content.text.data.replace(
-                  /a href="([^"]*\.[^"]*)"/g,
-                  `a href="${settings.apiPath}$1/download/file"`,
-                ),
-              }}
-            />
-          )}
-          <div id="forest-metadata-slot">{''}</div>
-        </Container>
-      )}
-      {/* </Grid.Column> */}
+            {content.image && (
+              <Image
+                className="document-image"
+                src={content.image.scales.thumb.download}
+                floated="right"
+              />
+            )}
+            {content.remoteUrl && (
+              <span>
+                The link address is:
+                <a href={content.remoteUrl}>{content.remoteUrl}</a>
+              </span>
+            )}
+            {content.text && (
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: content.text.data.replace(
+                    /a href="([^"]*\.[^"]*)"/g,
+                    `a href="${settings.apiPath}$1/download/file"`,
+                  ),
+                }}
+              />
+            )}
+            <div id="forest-metadata-slot">{''}</div>
+          </Container>
+        )}
+        {/* </Grid.Column> */}
 
-      {/* {renderPortletManager('plone.rightcolumn', 3, { ...props })} */}
-    </Grid>
+        {/* {renderPortletManager('plone.rightcolumn', 3, { ...props })} */}
+      </Grid>
+    </>
   );
 };
 
