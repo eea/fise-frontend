c4dfd5c85b8d61982e4cd2a797c297dfbd5f3c8e
razvanMiu <miu.razvan28@gmail.com>
2021-06-22 22:14:50 +0300

    Update

--- src/customizations/volto/components/theme/Forbidden/Forbidden.jsx
+++ src/customizations/volto/components/theme/Forbidden/Forbidden.jsx
@@ -1,72 +1,52 @@
-
-import React, { Component } from 'react';
-import { Portal } from 'react-portal';
-import { Route } from "react-router-dom";
-import { FormattedMessage } from 'react-intl';
 /**
- * @export
- * @class Forbidden
- * @extends {Component}
+ * @module components/theme/Forbidden/Forbidden
  */
-class Forbidden extends Component {
-    /**
-     * Render method.
-     * @method render
-     * @returns {string} Markup for the component.
-     */
-    render() {
-        return (
-            <Route render={({ staticContext }) => {
-                if (staticContext) staticContext.status = 403;
-                else staticContext = { status: 403 };
-                return (
-                    <div className="ui container view-wrapper">
-                        <h1>
-                            <FormattedMessage id="Forbidden" defaultMessage="Forbidden" />
-                        </h1>
-                        <p className="description">
-                            <FormattedMessage
-                                id="We apologize for the inconvenience, but you don't have permissions on this resource."
-                                defaultMessage="We apologize for the inconvenience, but you don't have permissions on this resource."
-                            />
-                        </p>
-                        <p>
-                            If you are certain you have the correct web address but are encountering an error, please contact the{" "}
-                            <a href="mailto:info@eea.europa.eu">
-                                Site Administration
-                            </a>
-                            .
-                        </p>
-                        <p>
-                            Or go back to {" "}
-                            <a href="/">
-                                Homepage
-                            </a>
-                            .
-                        </p>
-                        <p>
-                            Thank you.
-                        </p>
-                        {__CLIENT__ &&
-                            document.querySelector(
-                                '.header-image-wrapper .header-image-content',
-                            ) && (
-                                <Portal
-                                    node={
-                                        __CLIENT__ &&
-                                        document.querySelector(
-                                            '.header-image-wrapper .header-image-content',
-                                        )
-                                    }
-                                >
-                                    <h1 style={{ color: "white" }}>Forbidden</h1>
-                                </Portal>
-                            )}
-                    </div>
-                );
-            }}></Route>
-        );
-    }
-}
 
-export default Forbidden;
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Container } from 'semantic-ui-react';
+import { withServerErrorCode } from '@plone/volto/helpers/Utils/Utils';
+import { Portal } from 'react-portal';
+
+/**
+ * forbidden function.
+ * @function Forbidden
+ * @returns {string} Markup of the forbidden page.
+ */
+const Forbidden = () => (
+  <Container className="view-wrapper">
+    <h1>
+      <FormattedMessage id="Forbidden" defaultMessage="Forbidden" />
+    </h1>
+    <p className="description">
+      <FormattedMessage
+        id="We apologize for the inconvenience, but you don't have permissions on this resource."
+        defaultMessage="We apologize for the inconvenience, but you don't have permissions on this resource."
+      />
+    </p>
+    <p>
+      If you are certain you have the correct web address but are encountering
+      an error, please contact the{' '}
+      <a href="mailto:info@eea.europa.eu">Site Administration</a>.
+    </p>
+    <p>
+      Or go back to <a href="/">Homepage</a>.
+    </p>
+    <p>Thank you.</p>
+    {__CLIENT__ &&
+      document.querySelector('.header-image-wrapper .header-image-content') && (
+        <Portal
+          node={
+            __CLIENT__ &&
+            document.querySelector(
+              '.header-image-wrapper .header-image-content',
+            )
+          }
+        >
+          <h1 style={{ color: 'white' }}>Forbidden</h1>
+        </Portal>
+      )}
+  </Container>
+);
+
+export default withServerErrorCode(403)(Forbidden);
