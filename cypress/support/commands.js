b9077ba9b18f6d37b7e6cb5825d3dfda870bc5a7
razvanMiu <miu.razvan28@gmail.com>
2021-06-23 12:59:52 +0300

    Updated getIfExists cypress helper

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -302,10 +302,16 @@ function setBaseAndExtent(...args) {
   document.getSelection().setBaseAndExtent(...args);
 }
 
-function getIfExists(selector, action = () => {}) {
+function getIfExists(
+  selector,
+  successAction = () => {},
+  failAction = () => {},
+) {
   cy.get('body').then((body) => {
-    if (body.find(selector).length > 0) {
-      action();
+    if (body.find(selector).length > 0 && successAction) {
+      successAction();
+    } else if (failAction) {
+      failAction();
     }
   });
 }
