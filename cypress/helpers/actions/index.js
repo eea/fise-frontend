7f2d328255fe8dcab42dd7c1262fc1d28e312433
nileshgulia1 <nileshgulia@gmail.com>
2021-06-23 17:16:19 +0530

    correct save

--- cypress/helpers/actions/index.js
+++ cypress/helpers/actions/index.js
@@ -1,7 +1,8 @@
 export const save = (path) => {
   cy.url().then(($url) => {
     if ($url.includes(path)) {
-      cy.get('.ui.button.save').click();
+      //cy.get('.ui.button.save').click();
+      cy.get('#toolbar-save').click();
       cy.url().should('eq', Cypress.config().baseUrl + path);
     } else {
       cy.log('Wrong path');
