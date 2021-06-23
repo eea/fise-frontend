ff21667235751b83b8fbf60b43299bb1cec6b270
nileshgulia1 <nileshgulia@gmail.com>
2021-06-23 18:13:51 +0530

    wip

--- cypress/integration/block-basics.js
+++ cypress/integration/block-basics.js
@@ -5,26 +5,29 @@ describe('Blocks Tests', () => {
   beforeEach(setupBeforeEach);
   afterEach(tearDownAfterEach);
 
-  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
-    .clear()
-    .type('My Add-on Page')
-    .get('.documentFirstHeading span[data-text]')
-    .contains('My Add-on Page');
+  it('Add Block: Empty', () => {
+    // Change page title
+    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
+      .clear()
+      .type('My Add-on Page')
+      .get('.documentFirstHeading span[data-text]')
+      .contains('My Add-on Page');
 
-  cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
-    '{enter}',
-  );
+    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
+      '{enter}',
+    );
 
-  // Add block
-  cy.get('.ui.basic.icon.button.block-add-button').first().click();
-  cy.get('.blocks-chooser .title').contains('Media').click();
-  cy.get('.content.active.media .button.image').contains('Image').click();
+    // Add block
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Media').click();
+    cy.get('.content.active.media .button.image').contains('Image').click();
 
-  // Save
-  cy.get('#toolbar-save').click();
-  cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+    // Save
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
 
-  // then the page view should contain our changes
-  cy.contains('My Add-on Page');
-  cy.get('.block.image');
+    // then the page view should contain our changes
+    cy.contains('My Add-on Page');
+    cy.get('.block.image');
+  });
 });
