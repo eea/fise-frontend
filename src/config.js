52e1464d93a8a4a414be8509492f15401f5a068e
nileshgulia1 <nileshgulia@gmail.com>
2021-08-20 12:43:22 +0530

    shift theme components to @eeacms/volto-forest-theme

--- src/config.js
+++ src/config.js
@@ -1,64 +1,22 @@
-import Forbidden from '@plone/volto/components/theme/Forbidden/Forbidden';
-import Unauthorized from '@plone/volto/components/theme/Unauthorized/Unauthorized';
+/**
+ * Add your config changes here.
+ * @module config
+ * @example
+ * export default function applyConfig(config) {
+ *   config.settings = {
+ *     ...config.settings,
+ *     port: 4300,
+ *     listBlockTypes: {
+ *       ...config.settings.listBlockTypes,
+ *       'my-list-item',
+ *    }
+ * }
+ */
 
-import { installBlocks } from '@eeacms/volto-plotlycharts';
-import { applyConfig as installFiseFrontend } from './localconfig';
-
-import ObjectListInlineWidget from './components/manage/Widgets/ObjectListInlineWidget';
-import reducers from '~/reducers';
-
+// All your imports required for the config here BEFORE this line
 import '@plone/volto/config';
 
 export default function applyConfig(config) {
   // Add here your project's configuration here by modifying `config` accordingly
-  config = [installBlocks, installFiseFrontend].reduce(
-    (acc, apply) => apply(acc),
-    config,
-  );
-
-  config.settings = {
-    ...config.settings,
-    frontendMeta: {
-      version: process.env.RAZZLE_FRONTEND_VERSION || null,
-      version_url: process.env.RAZZLE_FRONTEND_VERSION_URL || null,
-      published_at: process.env.RAZZLE_FRONTEND_PUBLISHED_AT || null,
-    },
-    timezone: 'CET',
-    matomoSiteId: 46,
-    pathsWithFullobjects: ['/news', '/events'],
-    pathsWithExtraParameters: {
-      '/news': { b_start: 0, b_size: 1000 },
-      '/events': { b_start: 0, b_size: 1000 },
-    },
-  };
-
-  config.views = {
-    ...config.views,
-    errorViews: {
-      ...config.views.errorViews,
-      '403': Forbidden,
-      '401': Unauthorized,
-    },
-  };
-
-  config.widgets = {
-    ...config.widgets,
-    widget: {
-      ...config.widgets.widget,
-      object_list_inline: ObjectListInlineWidget,
-    },
-  };
-
-  config.viewlets = [...(config.viewlets || [])];
-  config.addonReducers = { ...config.addonReducers, ...reducers };
-
-  // export const portlets = {
-  //   ...config.portlets,
-  // };
-
-  config.editForms = {
-    ...config.editForms,
-  };
-
   return config;
 }
