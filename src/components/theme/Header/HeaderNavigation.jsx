5af431b76c1130036d9e192c86c9deba4d904468
Andrei Grigore <andrei.ggr@gmail.com>
2021-10-27 17:34:04 +0300

    Header Navigation logic

--- src/components/theme/Header/HeaderNavigation.jsx
+++ src/components/theme/Header/HeaderNavigation.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { useHistory } from 'react-router-dom';
 
 const HeaderNavigation = ({ items }) => {
-  const [activeItem, setActiveItems] = React.useState('');
-  const handleItemClick = (e, { name }) => setActiveItems(name);
+  const [activeItem, setActiveItem] = React.useState('');
+  const history = useHistory();
+
+  React.useEffect(() => {
+    const activeRouteDetected = items.filter(
+      (item) => item.url === history.location.pathname,
+    );
+    if (activeRouteDetected && activeRouteDetected.length > 0) {
+      setActiveItem(activeRouteDetected[0]);
+    }
+  }, []);
+
+  const handleNavigate = (item) => {
+    setActiveItem(item);
+    history.push(`${item.url}`);
+  };
 
   return (
     <div className="header-navigation-lead">
-      {items.length > 0 &&
-        items.map((item, index) => <div key={index}>{item.title}</div>)}
       <Menu tabular>
-        <Menu.Item
-          name="bio"
-          active={activeItem === 'bio'}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name="photos"
-          active={activeItem === 'photos'}
-          onClick={handleItemClick}
-        />
+        {items.length > 0 &&
+          items.map((item, index) => (
+            <Menu.Item
+              className="lead-navigation-item"
+              name={item.title}
+              active={activeItem.title === item.title}
+              onClick={() => handleNavigate(item)}
+            />
+          ))}
       </Menu>
     </div>
   );
