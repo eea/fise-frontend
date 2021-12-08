f77910967c020cad8b8326bfdbe43a536e624250
Andrei Grigore <andrei.ggr@gmail.com>
2021-12-08 13:50:56 +0200

    Add missing routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,10 @@
 import { App } from '@plone/volto/components';
 import { defaultRoutes } from '@plone/volto/routes';
 import config from '@plone/volto/registry';
+import SiteMap from '@eeacms/volto-forests-theme/components/theme/SiteMap/SiteMap';
+import Header from '@eeacms/volto-forests-theme/components/theme/CatalogueViews/AppHeader.jsx';
+import Footer from '@eeacms/volto-forests-theme/components/theme/CatalogueViews/AppFooter.jsx';
+import Head from '@eeacms/volto-forests-theme/components/theme/CatalogueViews/AppHead.jsx';
 
 /**
  * Routes array.
@@ -13,6 +17,23 @@ import config from '@plone/volto/registry';
  * @returns {array} Routes.
  */
 const routes = [
+  {
+    path: '/header',
+    component: Header,
+  },
+  {
+    path: '/footer',
+    component: Footer,
+  },
+  {
+    path: '/head',
+    component: Head,
+  },
+  {
+    path: '/sitemap',
+    component: SiteMap,
+    exact: true,
+  },
   {
     path: '/',
     component: App, // Change this if you want a different component
