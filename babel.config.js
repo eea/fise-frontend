525ac5bce3fae8334b58d8dcf5dcf2ef4ed6066c
andreiggr <andrei.ggr@gmail.com>
2022-11-04 00:24:36 +0200

    increase latest possible versions

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
-const baseCfg = require('@plone/volto/babel');
+module.exports = require('@plone/volto/babel');
 
-module.exports = function (api) {
-  const voltoConfig = baseCfg(api);
-  const presets = voltoConfig.presets;
-  const plugins = [...voltoConfig.plugins];
-  return {
-    plugins,
-    presets,
-    sourceType: 'unambiguous',
-  };
-};
+// module.exports = function (api) {
+//   const voltoConfig = baseCfg(api);
+//   const presets = voltoConfig.presets;
+//   const plugins = [...voltoConfig.plugins];
+//   return {
+//     plugins,
+//     presets,
+//     sourceType: 'unambiguous',
+//   };
+// };
