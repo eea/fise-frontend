e6a2944fb0cd45418041a19cd2c11ee80925ac0f
andreiggr <andrei.ggr@gmail.com>
2022-11-18 21:15:44 +0200

    entrypoint update

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -Ex
 
+if [ -z "$API_PATH" ]; then
+  API_PATH="http://localhost:8080/fise"
+fi
+
+if [ -z "$INTERNAL_API_PATH" ]; then
+  INTERNAL_API_PATH="http://backend:8080/fise"
+fi
+
 function apply_rebuild {
   mkdir -p /app/src/addons
   gosu node yarn develop
@@ -11,8 +19,26 @@ function apply_rebuild {
 # Should we re-build
 test -n "$REBUILD" && apply_rebuild
 
-# Sentry
-gosu node ./create-sentry-release.sh
+function apply_path {
+    mainjs=./build/server.js
+    bundlejs=./build/public/static/js/*.js
+    test -f $mainjs
+
+    echo "Check that we have API_PATH and API vars"
+    test -n "$API_PATH"
+
+    echo "Changing built files inplace"
+    sed -i "s#VOLTO_API_PATH#${API_PATH}#g" $mainjs
+    sed -i "s#VOLTO_API_PATH#${API_PATH}#g" $bundlejs
+    sed -i "s#VOLTO_INTERNAL_API_PATH#${INTERNAL_API_PATH}#g" $mainjs
+    sed -i "s#VOLTO_INTERNAL_API_PATH#${INTERNAL_API_PATH}#g" $bundlejs
+
+    echo "Zipping JS Files"
+    gzip -fk $mainjs
+}
+
+# Should we monkey patch?
+test -n "$API_PATH" && apply_path
 
 if [[ "$1" == "yarn"* ]]; then
   echo "Starting Volto"
