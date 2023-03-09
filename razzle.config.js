8e3ca10b85383c85e666ff1765ef746c1b8cc2a7
andreiggr <andrei.ggr@gmail.com>
2023-03-09 17:13:24 +0200

    add compression plugins

--- razzle.config.js
+++ razzle.config.js
@@ -4,6 +4,8 @@
  */
 
 const jsConfig = require('./jsconfig').compilerOptions;
+const CompressionPlugin = require('compression-webpack-plugin'); //gzip
+const BrotliPlugin = require('brotli-webpack-plugin'); //brotli
 
 const pathsConfig = jsConfig.paths;
 let voltoPath = './node_modules/@plone/volto';
@@ -14,3 +16,25 @@ Object.keys(pathsConfig).forEach((pkg) => {
 });
 
 module.exports = require(`${voltoPath}/razzle.config`);
+
+module.exports = {
+  ...module.exports,
+  plugins: [
+    ...module.exports.plugins,
+    new CompressionPlugin({
+      //gzip plugin
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8,
+    }),
+    new BrotliPlugin({
+      //brotli plugin
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
+};
