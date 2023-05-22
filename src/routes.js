e431ea9639f2945538358f4d0f71343e9dec4ad5
David Ichim <ichim.david@gmail.com>
2023-05-22 17:01:32 +0300

    change(site): cleaned routes from forest-frontend project routes
    - leave only the default routes from volto, if we need special routes
      we can have them within the addons

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,6 @@
 import { App } from '@plone/volto/components';
 import { defaultRoutes } from '@plone/volto/routes';
 import config from '@plone/volto/registry';
-import Header from '@eeacms/volto-forest-policy/components/theme/CatalogueViews/AppHeader.jsx';
-import Footer from '@eeacms/volto-forest-policy/components/theme/CatalogueViews/AppFooter.jsx';
-import Head from '@eeacms/volto-forest-policy/components/theme/CatalogueViews/AppHead.jsx';
 
 /**
  * Routes array.
@@ -16,18 +13,6 @@ import Head from '@eeacms/volto-forest-policy/components/theme/CatalogueViews/Ap
  * @returns {array} Routes.
  */
 const routes = [
-  {
-    path: '/header',
-    component: Header,
-  },
-  {
-    path: '/footer',
-    component: Footer,
-  },
-  {
-    path: '/head',
-    component: Head,
-  },
   {
     path: '/',
     component: App, // Change this if you want a different component
